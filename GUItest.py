4f99886056daa70c259ee4e1808ba7dc3a62578e
seannam1218 <seannam1218@gmail.com>
2017-12-21 00:39:23 -0800

    loops (create_arc) not working at all.

--- GUItest.py
+++ GUItest.py
@@ -20,39 +20,39 @@ canvas = Canvas(root, width = WIDTH, height = HEIGHT)
 vertexPosArray = []
 
 #drawing code goes here
-graph = Graph(15, 17)
+graph = Graph(6, 7)
 graph.makeGraph()
 
-def create_edge(x1, y1, x2, y2):
-    # atan computes in radians
-    if x2-x1 == 0:
-        dx = 0
-        dy = VERTEX_RADIUS
-    elif y2-y1 == 0:
-        dx = VERTEX_RADIUS
-        dy = 0
+# x1,y1,x2,y2, are vertex positions.
+def draw_edge(x1, y1, x2, y2):
+    if (round(x1, 3) == round(x2, 3)) and (round(y1, 3) == round(y2, 3)):
+        canvas.create_arc(x1+15, y1-15, x2+15, y2-15, start = 0, extent = 180)
     else:
-        angle = abs(atan((y2 - y1) / (x2 - x1)))
-        print("angle: ")
-        print(angle)
-        dx = abs(VERTEX_RADIUS * cos(angle))
-        print("dx, dy: ")
-        print(dx)
-        dy = abs(VERTEX_RADIUS * sin(angle))
-        print(dy)
-    if x1 < x2:
-        if y1 < y2:
-            canvas.create_line(x1 + dx, y1 + dy, x2 - dx, y2 - dy)
-        if y1 >= y2:
-            canvas.create_line(x1 + dx, y1 - dy, x2 - dx, y2 + dy)
-    if x1 >= x2:
-        if y1 < y2:
-            canvas.create_line(x1 - dx, y1 + dy, x2 + dx, y2 - dy)
-        if y1 >= y2:
-            canvas.create_line(x1 - dx, y1 - dy, x2 + dx, y2 + dy)
+        # atan computes in radians
+        if round(x2, 3) == round(x1, 3):
+            dx = 0
+            dy = VERTEX_RADIUS
+        elif round(y2, 3) == round(y1, 3):
+            dx = VERTEX_RADIUS
+            dy = 0
+        else:
+            angle = abs(atan((y2 - y1) / (x2 - x1)))
+            dx = abs(VERTEX_RADIUS * cos(angle))
+            dy = abs(VERTEX_RADIUS * sin(angle))
+
+        if x1 < x2:
+            if y1 < y2:
+                canvas.create_line(x1 + dx, y1 + dy, x2 - dx, y2 - dy)
+            if y1 >= y2:
+                canvas.create_line(x1 + dx, y1 - dy, x2 - dx, y2 + dy)
+        if x1 >= x2:
+            if y1 < y2:
+                canvas.create_line(x1 - dx, y1 + dy, x2 + dx, y2 - dy)
+            if y1 >= y2:
+                canvas.create_line(x1 - dx, y1 - dy, x2 + dx, y2 + dy)
 
 #x and y are coordinates of the center of vertex.
-def create_vertex(name, x, y, r):
+def draw_vertex(name, x, y, r):
     # draw vertices
     canvas.create_oval(x-r, y-r, x+r, y+r, fill = VERTEX_COLOR)
     # draw labels for the vertices
@@ -66,7 +66,7 @@ for i in range(0, graph.v):
     v_y = Y_CENTER - R * cos(radians(360 * i/graph.v))
     v_pos = [v_x, v_y]
     vertexPosArray.append(v_pos)
-    create_vertex(graph.vertexArray[i], X_CENTER + R * sin(radians(360 * i/graph.v)), Y_CENTER - R * cos(radians(360 * i/graph.v)), VERTEX_RADIUS)
+    draw_vertex(graph.vertexArray[i], X_CENTER + R * sin(radians(360 * i/graph.v)), Y_CENTER - R * cos(radians(360 * i/graph.v)), VERTEX_RADIUS)
 
 for e in graph.edgeArray:
     vertex1_index = ord(e[0])-97
@@ -75,7 +75,7 @@ for e in graph.edgeArray:
     y1 = vertexPosArray[vertex1_index][1]
     x2 = vertexPosArray[vertex2_index][0]
     y2 = vertexPosArray[vertex2_index][1]
-    create_edge(x1, y1, x2, y2)
+    draw_edge(x1, y1, x2, y2)
 
 graph.printGraph()
 
